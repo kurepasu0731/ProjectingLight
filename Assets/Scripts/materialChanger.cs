52c689287abf18546ac089d7dd7f780777f6268d
kurepasu0731 <kurepasu0731@gmail.com>
2016-06-13 15:14:27 +0900

    ドラえもんの表情アニメーション追加

---
+++ Assets/Scripts/materialChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class materialChanger : MonoBehaviour {
+
+    public Material doraMaterial;
+    public Texture[] doraTextures;
+
+    private float changeTime = 1;
+    private float nowTime = 0;
+    private int texNo = 0;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        nowTime += Time.deltaTime;
+        if (nowTime > changeTime)
+        {
+            nowTime = 0;
+            texNo++;
+            if (texNo >= doraTextures.Length) texNo = 0;
+            doraMaterial.mainTexture = doraTextures[texNo];
+        }
+    }
+}
