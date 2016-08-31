c18e789c389b7290246c111baa1141bccd0788f9
kurepasu0731 <kurepasu0731@gmail.com>
2016-08-31 20:08:08 +0900

    PinHoleエフェクト追加

---
+++ Assets/Scripts/PinHoleEffect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.ImageEffects
+{
+    //[ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    //[AddComponentMenu("Image Effects/Displacement/CameraDistortion")]
+    public class PinHoleEffect : PostEffectsBase
+    {
+
+        public Shader cameraShader = null;
+        private Material cameraMaterial = null;
+
+        public override bool CheckResources()
+        {
+            CheckSupport(false);
+            cameraMaterial = CheckShaderAndCreateMaterial(cameraShader, cameraMaterial);
+
+            if (!isSupported)
+                ReportAutoDisable();
+            return isSupported;
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (CheckResources() == false)
+            {
+                Graphics.Blit(source, destination);
+                return;
+            }
+            cameraMaterial.SetVector("_Color", new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            Graphics.Blit(source, destination, cameraMaterial);
+        }
+    }
+}
