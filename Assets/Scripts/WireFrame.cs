1b43fe821575bc50b6ec596f5d457eff4ebb3874
kurepasu0731 <kurepasu0731@gmail.com>
2016-11-02 14:29:23 +0900

    ワイヤーフレーム表現してみた

---
+++ Assets/Scripts/WireFrame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WireFrame : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        mf.mesh.SetIndices(mf.mesh.GetIndices(0), MeshTopology.Lines, 0);
+
+    }
+}
